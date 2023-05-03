7b74230100c27fe0e3c5ac66f9d3c07d49de4c11
Ulrik <ulrik.edeline@outlook.fr>
2023-05-03 23:57:40 +0200

    Added custom layer and child logic for DOM

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
 var custom_element_child = [];
 var custom_element = [];
+var custom_element_layer = [];
 var layer = 0;
 
 function _CreateDOM(type, queryAttribute, isChild) {
   let custom = document.createElement(type);
+  //Set id or classname
   if (queryAttribute && queryAttribute[0] == ".") {
     custom.classList.add(queryAttribute.substring(1))
   } else if (queryAttribute && queryAttribute[0] == "#") {
     custom.id = queryAttribute.substring(1)
   }
-  let ok = getPreviousParentElement()[0]
-  let previousParentElement = getPreviousParentElement()[1]
-  console.log("here", isChild, custom, custom_element, custom_element[custom_element.length - 1], custom_element_child[custom_element_child.length - 1], ok, previousParentElement)
-  custom_element_child.push(isChild)
-  if (isChild && custom_element[custom_element.length - 1] != null) {
-    custom_element[custom_element.length - 1].appendChild(custom)
+  //Find latest element of current layer
+  let lastElementOfLayer
+  //If first DOM element or layer is the body's layer, directly append to body
+  if ((layer == 0 && custom_element.length == 0) || (layer == 0 && layer < custom_element_layer[custom_element_layer.length-1])) {
+    document.body.appendChild(custom)
     custom_element.push(custom)
-    layer++
-  } else {
-    if (ok && layer > 0) {
-      console.log('a', previousParentElement, custom)
-      previousParentElement.appendChild(custom)
-      custom_element.push(custom)
-      custom_element_child[custom_element_child.length - 1] = true
+    custom_element_child.push(isChild)
+    custom_element_layer.push(layer)
+    return
+  }
+  if (custom_element[custom_element.length - 1] != null) {
+    if (isChild) {
+      lastElementOfLayer = findLastElementOfLayer(layer)
+      layer++
     } else {
-      custom_element.push(custom)
+      lastElementOfLayer = findLastElementOfLayer(layer-1)
     }
   }
+  //Find the custom_element equal to it and append current element
+  const equal = (element) => element == lastElementOfLayer;
+  let index = custom_element.findIndex(equal);
+  if (custom_element[index]) {
+    custom_element[index].appendChild(custom)
+    isChild = true
+  }
+  //find index and append to element
+  //Push current layer to array
+  custom_element.push(custom)
+  custom_element_child.push(isChild)
+  custom_element_layer.push(layer)
 }
 
 function _AddEvent(event,func,t) {
@@ -80,31 +94,47 @@ function _Text(classname,text) {
 }
 
 function _UpLayer(number) {
-  number = String(number)
   if (number == "*") {
     layer = 0
     return
-  }
-  for (i in number) {
-    if (layer > 0) {
-      layer-=1
+  } else if (number > 0) {
+    for (let i = number; i > 0; i--) {
+      if (layer > 0) {
+        layer-=1
+      }
     }
   }
 }
 
-function getPreviousParentElement() {
-  for (let i = custom_element_child.length - 1; i > 0; i--) {
-    console.log("b", custom_element[i-1], custom_element_child[i], custom_element_child.length)
-    if (!custom_element_child[i-1]) {
-      return [true, custom_element[i-1]]
+function findLastElementOfLayer(layer) {
+  if (layer < 0) {
+    layer = 0
+  }
+  for (let i = custom_element_layer.length; i > 0; i--) {
+    if (layer == custom_element_layer[i-1]) {
+      return custom_element[i-1]
     }
   }
-  return false
 }
 
 _CreateDOM("div",".container",false)
 _CreateDOM("h1",".title",true)
 _CreateDOM("input",".input",false)
+_CreateDOM("div")
+  _CreateDOM("input",".input1",true)
+  _CreateDOM("input",".input2",false)
+_UpLayer("*")
+_CreateDOM("input",".input3")
+  _CreateDOM("input",".input4",true)
+  _CreateDOM("input",".input5",false)
+  _CreateDOM("button",".button6",false)
+    _CreateDOM("button",".button7",true)
+    _CreateDOM("button",".button8",false)
+  _UpLayer(1)
+    _CreateDOM("button",".button9",true)
+_UpLayer(2)
+_CreateDOM("button",".button10")
+
 _Display("*")
 _Attr(".container","style","text-align:center;color:red;")
 _Attr(".input","placeholder","ToDo")
