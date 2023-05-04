7127a7f5ae70486896f065a8b976b12739d0d72b
Ulrik <ulrik.edeline@outlook.fr>
2023-05-04 16:52:26 +0200

    Added more functions for to-do list and started to organise functions

--- main.js
+++ main.js
@@ -1,327 +1,93 @@
-var custom_element_child = [];
-var custom_element = [];
-var custom_element_layer = [];
-var layer = 0;
+import * as quack from "./quack.js";
 
-function _CreateDOM(type, queryAttribute, isChild, appendToParent) {
-  let custom = document.createElement(type);
-  //Set id or classname
-  if (queryAttribute && queryAttribute[0] == ".") {
-    custom.classList.add(queryAttribute.substring(1))
-  } else if (queryAttribute && queryAttribute[0] == "#") {
-    custom.id = queryAttribute.substring(1)
-  }
-  if (appendToParent != null) {
-    let nodes = document.querySelectorAll(appendToParent);
-    if (nodes.length > 1) {
-      nodes[nodes.length-1].appendChild(custom);
+quack._CreateDOM("section",".todoapp",false)
+  quack._CreateDOM("header",".header",true)
+    quack._CreateDOM("h1", "#title",true)
+    quack._Text("#title","todos")
+    quack._CreateDOM("input",".new-todo",false)
+    quack._Attr(".new-todo","placeholder","What needs to be done?")
+    quack._Attr(".new-todo","id","new-todo")
+  quack._UpLayer(1)
+  quack._CreateDOM("section",".main",false)
+  quack._CreateDOM("input","#toggle-all",true)
+  quack._Attr("#toggle-all", "class", "toggle-all")
+  quack._Attr("#toggle-all","type","checkbox")
+  quack._CreateDOM("label","#mark",false)
+  quack._Attr("#mark","for","toggle-all")
+  quack._Text("#mark","Mark all as complete")
+  quack._CreateDOM("ul",".todo-list",false)
+  function updateCount() {
+    let count = quack._CountChildrenOfElement('.todo-list') - quack._CountChildrenOfElementWithClass(".todo-list", "completed")
+    if (count == 1) {
+      quack._Text(".todo-count", count + " item left")
     } else {
-      nodes[0].appendChild(custom);
+      quack._Text(".todo-count", count + " items left")
     }
-    _Clear();
-    _Display("*");
-    return
-  }
-  //Find latest element of current layer
-  let lastElementOfLayer
-  //If first DOM element or layer is the body's layer, directly append to body
-  if ((layer == 0 && custom_element.length == 0) || (layer == 0 && layer < custom_element_layer[custom_element_layer.length-1])) {
-    document.body.appendChild(custom)
-    custom_element.push(custom)
-    custom_element_child.push(isChild)
-    custom_element_layer.push(layer)
-    return
-  }
-  if (custom_element[custom_element.length - 1] != null) {
-    if (isChild) {
-      lastElementOfLayer = findLastElementOfLayer(layer)
-      layer++
+    if (quack._CountChildrenOfElementWithClass(".todo-list", "completed") > 0) {
+      quack._ShowElement(".clear-completed")
     } else {
-      lastElementOfLayer = findLastElementOfLayer(layer-1)
+      quack._HideElement(".clear-completed")
     }
   }
-  //Find the custom_element equal to it and append current element
-  const equal = (element) => element == lastElementOfLayer;
-  let index = custom_element.findIndex(equal);
-  if (custom_element[index]) {
-    custom_element[index].appendChild(custom)
-    isChild = true
-  }
-  //find index and append to element
-  //Push current layer to array
-  custom_element.push(custom)
-  custom_element_child.push(isChild)
-  custom_element_layer.push(layer)
-}
-
-function _AddEvent(event,func,t) {
-  var elems = document.querySelectorAll(t);
-  elems.forEach(elem => {
-    elem.addEventListener(event,func);
-  });
-}
-
-function _AddEventToLastOfElement(event,func,t) {
-  t = LastElementLogic("_AddEventToLastOfElement", t)
-  t.addEventListener(event,func);
-}
-
-function _RemoveEvent(event, func, t) {
-  var elems = document.querySelectorAll(t);
-  elems.forEach(elem => {
-    elem.removeEventListener(event,func);
-  });
-}
-
-function _Display(name) {
-  custom_element.forEach((element,index) => {
-    console.log(name, element, index)
-    if (name == "*" && !custom_element_child[index]) {
-      document.body.appendChild(element)
-    }
-  });
-}
-
-function _Attr(classname, attr, value) {
-  let elems = document.querySelectorAll(classname)
-  elems.forEach(element => {
-    element.setAttribute(attr, value);
-  });
-}
-
-function _Clear() {
-  custom_element_child = [];
-  custom_element = [];
-  layer = 0;
-}
-
-function _Undisplay(selector) {
-  var elements = document.querySelectorAll(selector);
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].parentNode.removeChild(elements[i]);
-  }
-}
-
-function _Text(classname,text) {
-  LastElementLogic("_Text", classname, text)
-}
-
-function _UpLayer(number) {
-  if (number == "*") {
-    layer = 0
-    return
-  } else if (number > 0) {
-    for (let i = number; i > 0; i--) {
-      if (layer > 0) {
-        layer-=1
-      }
-    }
-  }
-}
-
-function findLastElementOfLayer(layer) {
-  if (layer < 0) {
-    layer = 0
-  }
-  for (let i = custom_element_layer.length; i > 0; i--) {
-    if (layer == custom_element_layer[i-1]) {
-      return custom_element[i-1]
-    }
-  }
-}
-
-
-function _Get(element) {
-  return LastElementLogic("_Get", element)
-}
-
-function _ClearValue(element) {
-  LastElementLogic("_ClearValue", element)
-}
-
-function _DeleteParent(child) {
-      // Sélectionnez l'élément parent de l'élément enfant
-  var parent = child.parentElement;
-  if (parent !== null) {
-    parent.remove();
-  }
-}
-
-function LastElementLogic(funcName, element, text) {
-  let nodes = document.querySelectorAll(element);
-  if (funcName == "_Text") {
-    let nodes = document.querySelectorAll(element);
-    if (nodes.length > 1) {
-      return nodes[nodes.length-1].innerHTML = text;
-    } else {
-      return nodes[0].innerHTML = text;
-    }
-  } else if (funcName == "_Get") {
-    if (nodes.length > 1) {
-      return nodes[nodes.length-1].value;
-    } else {
-      return nodes[0].value;
-    }
-  } else if (funcName == "_ClearValue") {
-    if (nodes.length > 1) {
-      nodes[nodes.length-1].value = "";
-    } else {
-      nodes[0].value = "";
-    }
-  } else if (funcName == "_HideElement") {
-    if (nodes.length > 1) {
-      nodes[nodes.length-1].style.display = 'none';
-    } else {
-      nodes[0].style.display = 'none';
-    }
-  } else if (funcName == "_ShowElement") {
-    if (nodes.length > 1) {
-      nodes[nodes.length-1].style.display = 'block';
-    } else {
-      nodes[0].style.display = 'block';
-    }
-  } else if (funcName == "_CountChildrenOfElementWithClass" || funcName == "_AddEventToLastOfElement" || funcName == "_CountChildrenOfElement") {
-    if (nodes.length > 1) {
-      return nodes[nodes.length-1];
-    } else {
-      return nodes[0];
-    }
-  }
-}
-
-function _HideElement(element) {
-  LastElementLogic("_HideElement", element);
-}
-
-function _ShowElement(element) {
-  LastElementLogic("_ShowElement", element);
-}
-
-function _HideElements(element) {
-  document.querySelectorAll(element).style.display = 'none';
-}
-
-function _AddClass(element, className) {
-  element.classList.add(className)
-}
-
-function _RemoveClass(element, className) {
-  element.classList.remove(className)
-}
-
-function _HasClass(element, className) {
-  return element.classList.contains(className)
-}
-
-function _ChildrenOfElement(element) {
-  return element.children
-}
-
-function _CountChildrenOfElementWithClass(element, className) {
-  if (element[0] == ".") {
-    element = LastElementLogic("_CountChildrenOfElementWithClass", element)
-  } else if (element[0] == "#") {
-    element = document.querySelector(element)
-  }
-  let count = 0
-  for (let i = 0; i < element.children.length; i++) {
-      if (element.childNodes[i].classList.contains(className)) {
-        count++
-      }        
-  }
-  return count
-}
-
-function _CountChildrenOfElement(element) {
-  if (element[0] == ".") {
-    element = LastElementLogic("_CountChildrenOfElement", element)
-  } else if (element[0] == "#") {
-    element = document.querySelector(element)
-  }
-  return element.children.length
-}
-
-
-_CreateDOM("section",".todoapp",false)
-  _CreateDOM("header",".header",true)
-    _CreateDOM("h1", "#title",true)
-    _Text("#title","todos")
-    _CreateDOM("input",".new-todo",false)
-    _Attr(".new-todo","placeholder","What needs to be done?")
-    _Attr(".new-todo","id","new-todo")
-  _UpLayer(1)
-  _CreateDOM("section",".main",false)
-  _CreateDOM("input","#toggle-all",true)
-  _Attr("#toggle-all", "class", "toggle-all")
-  _Attr("#toggle-all","type","checkbox")
-  _CreateDOM("label","#mark",false)
-  _Attr("#mark","for","toggle-all")
-  _Text("#mark","Mark all as complete")
-  _CreateDOM("ul",".todo-list",false)
-  _AddEvent("keydown",function(e){
-    if (e.keyCode == 13) {
+  quack._AddEvent("keydown",function(e){
+    if (e.keyCode == 13 && quack._Get(".new-todo") != "") {
       console.log(e)
-      _ShowElement(".footer")
-      _ShowElement("#mark")
-      _CreateDOM("li",".l",true, ".todo-list")
-        _CreateDOM("div",".view",true, ".l")
-          _CreateDOM("input",".toggle",true, ".view")
-          _Attr(".toggle","type","checkbox")
-          _CreateDOM("label",".lbl",false, ".view")
-          _Text(".lbl",_Get(".new-todo"))
-          _ClearValue(".new-todo")
-          _CreateDOM("button",".destroy",false, ".view")
-          _AddEventToLastOfElement("change",function(){
+      quack._ShowElement(".footer")
+      quack._ShowElement("#mark")
+      quack._CreateDOM("li",".l",true, ".todo-list")
+        quack._CreateDOM("div",".view",true, ".l")
+          quack._CreateDOM("input",".toggle",true, ".view")
+          quack._Attr(".toggle","type","checkbox")
+          quack._CreateDOM("label",".lbl",false, ".view")
+          quack._Text(".lbl",quack._Get(".new-todo"))
+          quack._ClearValue(".new-todo")
+          quack._CreateDOM("button",".destroy",false, ".view")
+          quack._AddEventToLastOfElement("change",function(){
             //console.log("i")
             let list = this.parentElement.parentElement
-            if (_HasClass(list, "completed")) {
-              _RemoveClass(list, "completed")
+            if (quack._HasClass(list, "completed")) {
+              quack._RemoveClass(list, "completed")
             } else {
-              _AddClass(list, "completed")
+              quack._AddClass(list, "completed")
             }
-            let count = _CountChildrenOfElement('.todo-list') - _CountChildrenOfElementWithClass(".todo-list", "completed")
-            if (count == 1) {
-              _Text(".todo-count", count + " item left")
-            } else {
-              _Text(".todo-count", count + " items left")
-            }
+            updateCount()
           }, ".toggle")
-          let count = _CountChildrenOfElement('.todo-list') - _CountChildrenOfElementWithClass(".todo-list", "completed")
-          if (count == 1) {
-            _Text(".todo-count", count + " item left")
-          } else {
-            _Text(".todo-count", count + " items left")
-          }
-          _AddEvent("click",function(){
-            _DeleteParent(this)
+          updateCount()
+          quack._AddEvent("click",function(){
+            quack._DeleteParent(this.parentNode)
+            updateCount()
           },".destroy")
   
     }
   },"input")
-_UpLayer(1)
-_CreateDOM("footer",".footer",false)
-  _CreateDOM("span",".todo-count",true)
-  _Text(".todo-count", "1 item left")
-  _CreateDOM("ul",".filters",false)
-    _CreateDOM("li","",true)
-      _CreateDOM("a",".selected",true)
-      _Text(".selected","All")
-    _UpLayer(1)
-    _CreateDOM("li","",false)
-      _CreateDOM("a",".selected2",true)
-      _Text(".selected2","Active")
-    _UpLayer(1)
-    _CreateDOM("li","",false)
-      _CreateDOM("a",".selected3",true)
-      _Text(".selected3","Completed")
-  _UpLayer(2)
-  _CreateDOM("button",".clear-completed",false)
-  _Text(".selected3","Completed")
+quack._UpLayer(1)
+quack._CreateDOM("footer",".footer",false)
+  quack._CreateDOM("span",".todo-count",true)
+  quack._Text(".todo-count", "1 item left")
+  quack._CreateDOM("ul",".filters",false)
+    quack._CreateDOM("li","",true)
+      quack._CreateDOM("a",".selected",true)
+      quack._Text(".selected","All")
+    quack._UpLayer(1)
+    quack._CreateDOM("li","",false)
+      quack._CreateDOM("a",".selected2",true)
+      quack._Text(".selected2","Active")
+    quack._UpLayer(1)
+    quack._CreateDOM("li","",false)
+      quack._CreateDOM("a",".selected3",true)
+      quack._Text(".selected3","Completed")
+  quack._UpLayer(2)
+  quack._CreateDOM("button",".clear-completed",false)
+  //quack._Text(".selected3","Completed")
+  quack._Text(".clear-completed","Clear completed")
+  quack._AddEvent("click",function(){
+    quack._RemoveChildrenElementWithClass('.todo-list', 'completed')
+  },".clear-completed")
 
 if (document.querySelector(".todo-list").children.length == 0) {
-  _HideElement(".footer")
-  _HideElement("#mark")
+  quack._HideElement(".footer")
+  quack._HideElement("#mark")
 }
 
 
-_Display("*")
+quack._Display("*")
---
+++ quack.js
@@ -0,0 +1,263 @@
+var custom_element_child = [];
+var custom_element = [];
+var custom_element_layer = [];
+var layer = 0;
+
+export function _CreateDOM(type, queryAttribute, isChild, appendToParent) {
+    let custom = document.createElement(type);
+    //Set id or classname
+    if (queryAttribute && queryAttribute[0] == ".") {
+      custom.classList.add(queryAttribute.substring(1))
+    } else if (queryAttribute && queryAttribute[0] == "#") {
+      custom.id = queryAttribute.substring(1)
+    }
+    if (appendToParent != null) {
+      let nodes = document.querySelectorAll(appendToParent);
+      if (nodes.length > 1) {
+        nodes[nodes.length-1].appendChild(custom);
+      } else {
+        nodes[0].appendChild(custom);
+      }
+      _Clear();
+      _Display("*");
+      return
+    }
+    //Find latest element of current layer
+    let lastElementOfLayer
+    //If first DOM element or layer is the body's layer, directly append to body
+    if ((layer == 0 && custom_element.length == 0) || (layer == 0 && layer < custom_element_layer[custom_element_layer.length-1])) {
+      document.body.appendChild(custom)
+      custom_element.push(custom)
+      custom_element_child.push(isChild)
+      custom_element_layer.push(layer)
+      return
+    }
+    if (custom_element[custom_element.length - 1] != null) {
+      if (isChild) {
+        lastElementOfLayer = findLastElementOfLayer(layer)
+        layer++
+      } else {
+        lastElementOfLayer = findLastElementOfLayer(layer-1)
+      }
+    }
+    //Find the custom_element equal to it and append current element
+    const equal = (element) => element == lastElementOfLayer;
+    let index = custom_element.findIndex(equal);
+    if (custom_element[index]) {
+      custom_element[index].appendChild(custom)
+      isChild = true
+    }
+    //find index and append to element
+    //Push current layer to array
+    custom_element.push(custom)
+    custom_element_child.push(isChild)
+    custom_element_layer.push(layer)
+  }
+  
+  export function _AddEvent(event,func,t) {
+    var elems = document.querySelectorAll(t);
+    elems.forEach(elem => {
+      elem.addEventListener(event,func);
+    });
+  }
+  
+  export function _AddEventToLastOfElement(event,func,t) {
+    t = LastElementLogic("_AddEventToLastOfElement", t)
+    t.addEventListener(event,func);
+  }
+  
+  export function _RemoveEvent(event, func, t) {
+    var elems = document.querySelectorAll(t);
+    elems.forEach(elem => {
+      elem.removeEventListener(event,func);
+    });
+  }
+  
+  export function _Display(name) {
+    custom_element.forEach((element,index) => {
+      console.log(name, element, index)
+      if (name == "*" && !custom_element_child[index]) {
+        document.body.appendChild(element)
+      }
+    });
+  }
+  
+  export function _Attr(classname, attr, value) {
+    let elems = document.querySelectorAll(classname)
+    elems.forEach(element => {
+      element.setAttribute(attr, value);
+    });
+  }
+  
+  export function _Clear() {
+    custom_element_child = [];
+    custom_element = [];
+    layer = 0;
+  }
+  
+  export function _Undisplay(selector) {
+    var elements = document.querySelectorAll(selector);
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].parentNode.removeChild(elements[i]);
+    }
+  }
+  
+  export function _Text(classname,text) {
+    LastElementLogic("_Text", classname, text)
+  }
+  
+  export function _UpLayer(number) {
+    if (number == "*") {
+      layer = 0
+      return
+    } else if (number > 0) {
+      for (let i = number; i > 0; i--) {
+        if (layer > 0) {
+          layer-=1
+        }
+      }
+    }
+  }
+  
+  export function findLastElementOfLayer(layer) {
+    if (layer < 0) {
+      layer = 0
+    }
+    for (let i = custom_element_layer.length; i > 0; i--) {
+      if (layer == custom_element_layer[i-1]) {
+        return custom_element[i-1]
+      }
+    }
+  }
+  
+  
+  export function _Get(element) {
+    return LastElementLogic("_Get", element)
+  }
+  
+  export function _ClearValue(element) {
+    LastElementLogic("_ClearValue", element)
+  }
+  
+  export function _DeleteParent(child) {
+        // Sélectionnez l'élément parent de l'élément enfant
+    var parent = child.parentElement;
+    if (parent !== null) {
+      parent.remove();
+    }
+  }
+  
+  export function LastElementLogic(funcName, element, text) {
+    let nodes = document.querySelectorAll(element);
+    if (funcName == "_Text") {
+      let nodes = document.querySelectorAll(element);
+      if (nodes.length > 1) {
+        return nodes[nodes.length-1].innerHTML = text;
+      } else {
+        return nodes[0].innerHTML = text;
+      }
+    } else if (funcName == "_Get") {
+      if (nodes.length > 1) {
+        return nodes[nodes.length-1].value;
+      } else {
+        return nodes[0].value;
+      }
+    } else if (funcName == "_ClearValue") {
+      if (nodes.length > 1) {
+        nodes[nodes.length-1].value = "";
+      } else {
+        nodes[0].value = "";
+      }
+    } else if (funcName == "_HideElement") {
+      if (nodes.length > 1) {
+        nodes[nodes.length-1].style.display = 'none';
+      } else {
+        nodes[0].style.display = 'none';
+      }
+    } else if (funcName == "_ShowElement") {
+      if (nodes.length > 1) {
+        nodes[nodes.length-1].style.display = 'block';
+      } else {
+        nodes[0].style.display = 'block';
+      }
+    } else if (funcName == "_CountChildrenOfElementWithClass" || funcName == "_AddEventToLastOfElement" || funcName == "_CountChildrenOfElement" || funcName == "_RemoveElementWithClass") {
+      if (nodes.length > 1) {
+        return nodes[nodes.length-1];
+      } else {
+        return nodes[0];
+      }
+    }
+  }
+  
+  export function _HideElement(element) {
+    LastElementLogic("_HideElement", element);
+  }
+  
+  export function _ShowElement(element) {
+    LastElementLogic("_ShowElement", element);
+  }
+  
+  export function _HideElements(element) {
+    document.querySelectorAll(element).style.display = 'none';
+  }
+  
+  export function _AddClass(element, className) {
+    element.classList.add(className)
+  }
+  
+  export function _RemoveClass(element, className) {
+    element.classList.remove(className)
+  }
+  
+  export function _HasClass(element, className) {
+    return element.classList.contains(className)
+  }
+  
+  export function _ChildrenOfElement(element) {
+    return element.children
+  }
+  
+  export function _CountChildrenOfElementWithClass(element, className) {
+    if (element[0] == ".") {
+      element = LastElementLogic("_CountChildrenOfElementWithClass", element)
+    } else if (element[0] == "#") {
+      element = document.querySelector(element)
+    }
+    let count = 0
+    for (let i = 0; i < element.children.length; i++) {
+        if (element.childNodes[i].classList.contains(className)) {
+          count++
+        }        
+    }
+    return count
+  }
+  
+  export function _CountChildrenOfElement(element) {
+    if (element[0] == ".") {
+      element = LastElementLogic("_CountChildrenOfElement", element)
+    } else if (element[0] == "#") {
+      element = document.querySelector(element)
+    }
+    return element.children.length
+  }
+  
+  export function _SetElementColor(element, color) {
+    element.style.color = color
+  }
+
+  export function _RemoveChildrenElementWithClass(element, className) {
+    let toDelete = []
+    if (element[0] == ".") {
+        element = LastElementLogic("_RemoveElementWithClass", element)
+      } else if (element[0] == "#") {
+        element = document.querySelector(element)
+      }
+      for (let i = 0; i < element.childNodes.length; i++) {
+          if (element.childNodes[i].classList.contains(className)) {
+              toDelete.push(element.childNodes[i])
+            }        
+        }
+        for (let i = 0; i < toDelete.length; i++) {
+          toDelete[i].remove()
+        }
+  }
